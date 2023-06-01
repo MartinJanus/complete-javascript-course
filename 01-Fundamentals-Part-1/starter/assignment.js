7f299ffa8e9057743b851a54ab2036a9067481d1
Martin Janus <martinasjanuska@gmail.com>
2023-06-01 22:40:44 +0100

    data types

--- 01-Fundamentals-Part-1/starter/assignment.js
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -6,3 +6,13 @@ let population = "5 Million";
 
 
 console.log(country + continent + population);
+
+//assignment Data Types 
+
+let isIsland = false;
+
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof language);
+console.log(typeof population);
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,5 @@
+//Value and Variables 
+
 let js = "Amazing";
 console.log(40 + 8 + 23 - 10);
 
@@ -7,4 +9,24 @@ console.log(23)
 let firstName = "Matilda";
 
 console.log(firstName);
+
+//Data Types
+
+//delcared
+let jsisFun = true;
+
+console.log(jsisFun);
+
+console.log(typeof jsisFun);
+
+
+//changed data type (dynamic typing)
+jsisFun = "YES!";
+
+console.log(typeof jsisFun);
+
+//undefined 
+let year;
+
+console.log(typeof year);
 

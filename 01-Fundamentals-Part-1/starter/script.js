1a5d4dfe1c499cc34c493e5ff1aed077c7acab1f
Martin Janus <martinasjanuska@gmail.com>
2023-06-05 23:20:18 +0100

    logical operators

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -188,4 +188,32 @@ console.log(firstName);
 // }
 
 
+//Logical Operators 
+const hasDriversLicence = true; //A
+const hasGoodVision = true; //B
+
+console.log(hasDriversLicence && hasGoodVision);
+console.log(hasDriversLicence || hasGoodVision);
+console.log(!hasDriversLicence);
+
+const shouldDrive = hasDriversLicence && hasGoodVision;
+
+// if (hasDriversLicence && hasGoodVision) {
+//     console.log("Sarah is able to drive");
+// }
+// else {
+//     console.log("Someone else should drive");
+// }
+
+const isTired = false; //C
+console.log(hasDriversLicence || hasGoodVision || isTired);
+
+if (hasDriversLicence && hasGoodVision && !isTired) {
+    console.log("Sarah is able to drive");
+}
+else {
+    console.log("Someone else should drive");
+}
+
+
 
